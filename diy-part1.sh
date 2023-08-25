208f06a8041aceb6a08900bc22ae7a97d18f3d9a
Plutonium141 <62324696+Plutonium141@users.noreply.github.com>
2023-08-25 16:42:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+sed -i '28s/3568/3588/' package/boot/arm-trusted-firmware-rockchip-vendor/pack-firmware.sh
